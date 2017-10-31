8660a6fea120310339f56d217cb03a6743447141
minhthai <minhthai40@gmail.com>
2017-11-01 07:47:09 +0800

    More exercises in chapter 3

--- chapter3/src/main/scala/exercise_1/Exercise1.scala
+++ chapter3/src/main/scala/exercise_1/Exercise1.scala
@@ -21,13 +21,25 @@ object Exercise1 {
 
     assert(List.reverse(l) == List(5, 3, 2, 1))
 
-    assert(List.foldRight(l, "")((a, b) => s"$a & $b") == "1 & 2 & 3 & 5 & ")
-
     assert(List.sum(l) == 11)
 
     assert(List.product(l) == 30)
 
     assert(List.length(l) == 4)
+
+    assert(List.foldRight(l, "#")((a, b) => s"$a & $b") == "1 & 2 & 3 & 5 & #")
+
+    assert(List.foldLeft(l, "#")((a, b) => s"$a & $b") == "# & 1 & 2 & 3 & 5")
+
+    assert(List.reverse2(l) == List(5, 3, 2, 1))
+
+    assert(List.foldRight2(l, "#")((a, b) => s"$a & $b") == "1 & 2 & 3 & 5 & #")
+
+    assert(List.append(l, List(-10, 99)) == List(1, 2, 3, 5, -10, 99))
+
+    assert(List.append2(l, List(-10, 99)) == List(1, 2, 3, 5, -10, 99))
+
+    assert(List.concat(List(l, List(0, -6), l)) == List(1, 2, 3, 5, 0, -6, 1, 2, 3, 5))
   }
 }
 
@@ -95,6 +107,9 @@ object List {
     reverse(withoutLast)
   }
 
+  /**
+    * Reverse the list.
+    */
   def reverse[A](l: List[A]): List[A] = {
     @annotation.tailrec
     def loop(acc: List[A], cur: List[A]): List[A] = cur match {
@@ -104,8 +119,14 @@ object List {
     loop(Nil, l)
   }
 
+  def sum(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
+
+  def product(l: List[Int]): Int = foldLeft(l, 1)(_ * _)
+
+  def length[A](l: List[A]): Int = foldLeft(l, 0)((cnt, _) => cnt + 1)
+
   /**
-    * Applies a `f` to all elements (from right to left) and a start value `z`.
+    * Applies an operator `f` to all elements and a start value `z` (from right to left).
     */
   def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B =
     l match {
@@ -113,9 +134,47 @@ object List {
       case Elem(h, t) => f(h, foldRight(t, z)(f))
     }
 
-  def sum(l: List[Int]): Int = foldRight(l, 0)(_ + _)
+  /**
+    * Applies an operator `f` to a start value `z` and all elements (from left to right)
+    */
+  @annotation.tailrec
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match {
+      case Nil => z
+      case Elem(h, t) => foldLeft(t, f(z, h))(f)
+    }
 
-  def product(l: List[Int]): Int = foldRight(l, 1)(_ * _)
+  /**
+    * Same as `reverse` but implemented using `foldLeft`.
+    */
+  def reverse2[A](l: List[A]): List[A] = foldLeft[A, List[A]](l, Nil)((t, h) => Elem(h, t))
 
-  def length[A](l: List[A]): Int = foldRight(l, 0)((_, cnt) => 1 + cnt)
+  /**
+    * Same as `foldRight` but implemented using `foldLeft`.
+    * This is important because `foldLeft` is tail-recursive and can be optimized into iterations
+    * by the compiler. The old `foldRight` doesn't have this benefit.
+    */
+  def foldRight2[A, B](l: List[A], z: B)(f: (A, B) => B): B = {
+    foldLeft(reverse2(l), z)((a, b) => f(b, a))
+  }
+
+  /**
+    * Append a new element to the end of the list.
+    * Note that this method will traverse the whole list.
+    */
+  def append[A](l: List[A], other: List[A]): List[A] = {
+    foldRight2(l, other)((t, h) => Elem(t, h))
+  }
+
+  /**
+    * Same as `append` but implemented using `foldLeft`.
+    * This implementation is less efficient because it calls `foldLeft` twice.
+    */
+  def append2[A](l: List[A], other: List[A]): List[A] = {
+    foldLeft(reverse(l), other)((t, h) => Elem(h, t))
+  }
+
+  def concat[A](lists: List[List[A]]): List[A] = {
+    foldRight(lists, Nil : List[A])(append)
+  }
 }
