1bf4da0c09bc5cdea730c95c72139ec8a5cba380
minhthai <phammt@garena.com>
2017-12-19 09:00:05 +0800

    Add chapter 4, 5, 6

---
+++ chapter5/src/main/scala/exercise/Stream.scala
@@ -0,0 +1,16 @@
+package chapter5.stream
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+sealed trait Stream[+A]
+case object Empty extends Stream[Nothing]
+
+/**
+  * A stream element. `head` and `tail` only store how to compute its content, not the actually value.
+  * 'Cons' = 'Construct'
+  */
+case object Cons[+A](head: () => A, tail: () => Stream[A]) extends Stream[A]
